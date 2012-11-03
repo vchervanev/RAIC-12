c485e74f53006424f72da6eed1ca9dad926a85d1
Didoo <Didoo Master>
2012-11-04 01:12:45 +0400

    Speed test

---
+++ src/main/java/RandomMove.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 03.11.12
+ * Time: 22:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class RandomMove extends Action {
+
+    @Override
+    public void estimate() {
+        variant = Variant.none;
+    }
+
+    @Override
+    public void perform() {
+        env.directMoveTo(env.self.getHeight(),env.self.getHeight());
+    }
+}
